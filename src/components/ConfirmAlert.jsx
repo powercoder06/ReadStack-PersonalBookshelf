8257c9a1bc891fe1b247f59d3690dcfebbc2df6c
Ayush Kumar <bhaiyaayush2020@gmail.com>
2025-12-13 17:22:36 +0530

    Fixed Readability and maintainability issues detected

--- src/components/ConfirmAlert.jsx
+++ src/components/ConfirmAlert.jsx
@@ -1,258 +1,96 @@
 import Swal from "sweetalert2";
 
+const getTheme = (darkMode) => ({
+   background: darkMode ? "#151519" : "#ededed",
+   color: darkMode ? "#c20aff" : "#5d0085",
+   confirmButtonColor: darkMode ? "#c20aff" : "#5d0085",
+   cancelButtonColor: darkMode ? "#c20aff" : "#5d0085",
+});
+
+const createAlert = (darkMode, config) => {
+   const theme = getTheme(darkMode);
+   return Swal.fire({ ...theme, ...config });
+};
+
 export const deleteOneItemAlert = (darkMode, item) =>
-   Swal.fire(
-      darkMode
-         ? {
-              width: "20rem",
-              text: `Do you want to delete this ${item}?`,
-              background: "#151519",
-              color: "#c20aff",
-              showCancelButton: true,
-              confirmButtonColor: "#c20aff",
-              cancelButtonColor: "#c20aff",
-              confirmButtonText: "Yes, delete it!",
-           }
-         : {
-              width: "20rem",
-              text: `Do you want to delete this ${item}?`,
-              background: "#ededed",
-              color: "#5d0085",
-              showCancelButton: true,
-              confirmButtonColor: "#5d0085",
-              cancelButtonColor: "#5d0085",
-              confirmButtonText: "Yes, delete it!",
-           }
-   );
+   createAlert(darkMode, {
+      width: "20rem",
+      text: `Do you want to delete this ${item}?`,
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+   });
 
 export const deleteOneItemConfirmed = (darkMode, item) =>
-   Swal.fire(
-      darkMode
-         ? {
-              width: "20rem",
-              text: `The ${item} has been deleted.`,
-              background: "#151519",
-              color: "#c20aff",
-              confirmButtonColor: "#c20aff",
-           }
-         : {
-              width: "20rem",
-              text: `The ${item} has been deleted.`,
-              background: "#ededed",
-              color: "#5d0085",
-              confirmButtonColor: "#5d0085",
-           }
-   );
+   createAlert(darkMode, {
+      width: "20rem",
+      text: `The ${item} has been deleted.`,
+   });
 
 export const deleteAllItemsAlert = (darkMode, items) =>
-   Swal.fire(
-      darkMode
-         ? {
-              width: "22rem",
-              text: `Do you want to delete all the ${items}?`,
-              background: "#151519",
-              color: "#c20aff",
-              showCancelButton: true,
-              confirmButtonColor: "#c20aff",
-              cancelButtonColor: "#c20aff",
-              confirmButtonText: "Yes, delete all!",
-           }
-         : {
-              width: "22rem",
-              text: `Do you want to delete all the ${items}?`,
-              background: "#ededed",
-              color: "#5d0085",
-              showCancelButton: true,
-              confirmButtonColor: "#5d0085",
-              cancelButtonColor: "#5d0085",
-              confirmButtonText: "Yes, delete all!",
-           }
-   );
+   createAlert(darkMode, {
+      width: "22rem",
+      text: `Do you want to delete all the ${items}?`,
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete all!",
+   });
 
 export const deleteAllItemsConfirm = (darkMode, items) =>
-   Swal.fire(
-      darkMode
-         ? {
-              width: "20rem",
-              text: `all the ${items} have been deleted.`,
-              background: "#151519",
-              color: "#c20aff",
-              confirmButtonColor: "#c20aff",
-           }
-         : {
-              width: "20rem",
-              text: `all the ${items} have been deleted.`,
-              background: "#ededed",
-              color: "#5d0085",
-              confirmButtonColor: "#5d0085",
-           }
-   );
+   createAlert(darkMode, {
+      width: "20rem",
+      text: `all the ${items} have been deleted.`,
+   });
 
 export const deleteOneBookWithNoteAlert = (darkMode) =>
-   Swal.fire(
-      darkMode
-         ? {
-              width: "23rem",
-              title: "This book have notes!",
-              text: `Do you want to delete this book? notes will be deleted`,
-              background: "#151519",
-              color: "#c20aff",
-              showCancelButton: true,
-              confirmButtonColor: "#c20aff",
-              cancelButtonColor: "#c20aff",
-              confirmButtonText: "Yes, delete it!",
-           }
-         : {
-              width: "23rem",
-              title: "This book have notes!",
-              text: `Do you want to delete this book? notes will be deleted`,
-              background: "#ededed",
-              color: "#5d0085",
-              showCancelButton: true,
-              confirmButtonColor: "#5d0085",
-              cancelButtonColor: "#5d0085",
-              confirmButtonText: "Yes, delete it!",
-           }
-   );
+   createAlert(darkMode, {
+      width: "23rem",
+      title: "This book have notes!",
+      text: `Do you want to delete this book? notes will be deleted`,
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+   });
 
 export const deleteAllBooksWithNoteAlert = (darkMode) =>
-   Swal.fire(
-      darkMode
-         ? {
-              width: "28rem",
-              title: "There are books with notes!",
-              text: `Do you want to delete all the books? notes will be deleted`,
-              background: "#151519",
-              color: "#c20aff",
-              showCancelButton: true,
-              confirmButtonColor: "#c20aff",
-              cancelButtonColor: "#c20aff",
-              confirmButtonText: "Yes, delete all!",
-           }
-         : {
-              width: "28rem",
-              title: ["There are books with notes!"],
-              text: `Do you want to delete all the books? notes will be deleted`,
-              background: "#ededed",
-              color: "#5d0085",
-              showCancelButton: true,
-              confirmButtonColor: "#5d0085",
-              cancelButtonColor: "#5d0085",
-              confirmButtonText: "Yes, delete all!",
-           }
-   );
+   createAlert(darkMode, {
+      width: "28rem",
+      title: "There are books with notes!",
+      text: `Do you want to delete all the books? notes will be deleted`,
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete all!",
+   });
 
 export const deleteAllArchivedBooksAlert = (darkMode) =>
-   Swal.fire(
-      darkMode
-         ? {
-              width: "28rem",
-              title: "These books have notes!",
-              text: `Do you want to delete all the books? notes will be deleted`,
-              background: "#151519",
-              color: "#c20aff",
-              showCancelButton: true,
-              confirmButtonColor: "#c20aff",
-              cancelButtonColor: "#c20aff",
-              confirmButtonText: "Yes, delete all!",
-           }
-         : {
-              width: "28rem",
-              title: "These books have notes!",
-              text: `Do you want to delete all the books? notes will be deleted`,
-              background: "#ededed",
-              color: "#5d0085",
-              showCancelButton: true,
-              confirmButtonColor: "#5d0085",
-              cancelButtonColor: "#5d0085",
-              confirmButtonText: "Yes, delete all!",
-           }
-   );
+   createAlert(darkMode, {
+      width: "28rem",
+      title: "These books have notes!",
+      text: `Do you want to delete all the books? notes will be deleted`,
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete all!",
+   });
 
 export const archiveBookAlert = (darkMode) =>
-   Swal.fire(
-      darkMode
-         ? {
-              width: "22rem",
-              text: `Do you want to archive this book?`,
-              background: "#151519",
-              color: "#c20aff",
-              showCancelButton: true,
-              confirmButtonColor: "#c20aff",
-              cancelButtonColor: "#c20aff",
-              confirmButtonText: "Yes, archive it!",
-           }
-         : {
-              width: "22rem",
-              text: `Do you want to archive this book?`,
-              background: "#ededed",
-              color: "#5d0085",
-              showCancelButton: true,
-              confirmButtonColor: "#5d0085",
-              cancelButtonColor: "#5d0085",
-              confirmButtonText: "Yes, archive it!",
-           }
-   );
+   createAlert(darkMode, {
+      width: "22rem",
+      text: `Do you want to archive this book?`,
+      showCancelButton: true,
+      confirmButtonText: "Yes, archive it!",
+   });
 
 export const archiveBookConfirm = (darkMode) =>
-   Swal.fire(
-      darkMode
-         ? {
-              width: "20rem",
-              text: `The book has been archived.`,
-              background: "#151519",
-              color: "#c20aff",
-              confirmButtonColor: "#c20aff",
-           }
-         : {
-              width: "20rem",
-              text: `The book has been archived.`,
-              background: "#ededed",
-              color: "#5d0085",
-              confirmButtonColor: "#5d0085",
-           }
-   );
+   createAlert(darkMode, {
+      width: "20rem",
+      text: `The book has been archived.`,
+   });
 
 export const moveBackBookAlert = (darkMode) =>
-   Swal.fire(
-      darkMode
-         ? {
-              width: "22rem",
-              text: `Do you want to move back this book to the current reading?`,
-              background: "#151519",
-              color: "#c20aff",
-              showCancelButton: true,
-              confirmButtonColor: "#c20aff",
-              cancelButtonColor: "#c20aff",
-              confirmButtonText: "Yes, move it back!",
-           }
-         : {
-              width: "22rem",
-              text: `Do you want to move back this book to the current reading?`,
-              background: "#ededed",
-              color: "#5d0085",
-              showCancelButton: true,
-              confirmButtonColor: "#5d0085",
-              cancelButtonColor: "#5d0085",
-              confirmButtonText: "Yes, move it back!",
-           }
-   );
+   createAlert(darkMode, {
+      width: "22rem",
+      text: `Do you want to move back this book to the current reading?`,
+      showCancelButton: true,
+      confirmButtonText: "Yes, move it back!",
+   });
 
 export const moveBackBookConfirm = (darkMode) =>
-   Swal.fire(
-      darkMode
-         ? {
-              width: "20rem",
-              text: `The book has been moved back to the current reading.`,
-              background: "#151519",
-              color: "#c20aff",
-              confirmButtonColor: "#c20aff",
-           }
-         : {
-              width: "20rem",
-              text: `The book has been moved back to the current reading.`,
-              background: "#ededed",
-              color: "#5d0085",
-              confirmButtonColor: "#5d0085",
-           }
-   );
+   createAlert(darkMode, {
+      width: "20rem",
+      text: `The book has been moved back to the current reading.`,
+   });
